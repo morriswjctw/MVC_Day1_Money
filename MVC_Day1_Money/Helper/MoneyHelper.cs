fe56e826fff3e0428953af7d5ddf69200a83b8bd
morriswjctw <morris.wjc.tw@gmail.com>
2016-04-29 11:18:40 +0800

    列表加入顏色變換 類型的「支出」字樣為紅色，「收入」字樣為藍色

---
+++ MVC_Day1_Money/Helper/MoneyHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Day1_Money.Helper
+{
+    public static class MoneyHelper
+    {
+        public static MvcHtmlString SpenClasscolor(this HtmlHelper helper, string SpenClass)
+        {
+            if (SpenClass == "支出")
+                return MvcHtmlString.Create("<span style = \"color:red\" >支出</ span>");
+            else
+                return MvcHtmlString.Create(String.Format(@"<span style = ""color:blue"">{0}</ span>", SpenClass));
+        }
+    }
+}
